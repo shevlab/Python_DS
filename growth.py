ac9c7cdf9fce1f89de9b62a39c958734a0bf9368
shevlab <68078045+shevlab@users.noreply.github.com>
2020-07-30 13:08:13 +0300

    median

--- growth.py
+++ growth.py
@@ -1,6 +1,7 @@
 from statistics import median
-#data = (180, 183, 167, 175, 184, 170, 176 )
+import numpy as np
+data = (180, 183, 167, 175, 184, 170, 176 )
 
 print(median([180, 183, 167, 175, 184, 170, 176 ]))
 
-
+print(np.std(data))
